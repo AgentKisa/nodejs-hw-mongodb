5a28390ecdb4e970b0c3fb0f2e99f0b5cd7d1d11
AgentKisa <155394878+AgentKisa@users.noreply.github.com>
2024-09-14 17:57:40 +0300

    mongo-contacts

---
+++ src/server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import dotenv from 'dotenv';
+import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+dotenv.config();
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  app.get('/contacts', (req, res) => {
+    const contacts = getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', (req, res) => {
+    const { contactId } = req.params;
+    const contact = getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
+      data: { contact },
+    });
+  });
+
+  app.use;
+  '*',
+    (req, res) => {
+      res.status(404).json({ message: 'Not found' });
+    };
+};
